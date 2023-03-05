bffefece3e7804c0eebcc70866c22e7418f6fca6
Maxen <maxenvarghese@gmail.com>
2023-03-05 19:38:35 +0530

    Day Two

---
+++ variables_and_datatypes/bin/example4.dart
@@ -0,0 +1,17 @@
+//Day two mainly dealt with rest of datatypes ,some types of variables and some constraints to dealt when assigning variables.
+
+///They are,
+4.Assigning variables, datatype not mentioned instead "var" is used.
+5.Assigning to const values, cont values can be assigned to final but not vice versa.
+6.Inferring datatypes, eg: in const no need to specify datatype, they infer datatype automatically, "const myAge = 20" and "const int myAge = 20" both are valid.
+7.Mutating of const content.
+8.Final allow mutating of varialbe content.
+9.Assign const & final.
+10.Late variable.
+11.Data types.
+12.Type promotion, less precise value to high precise value, ie int to double, vice versa is called Type demoting, ie double to int.
+13.Specifying datatypes, apply analysis_option.yaml  "always_specify_types"  to help indicate to annote datatype , if user forget to mention.
+14.Naming conventions,  ie camelcase  eg:myAge,yourName ,(we can ignore: non_constant_identifier_names), and lowercamelcase for constant naming(we can ignore: constant_identifier_names).
+15.various datatypes, ie int,double,string,bool,list,map,set,dynamic,symbol.
+
+//End of day Two.
